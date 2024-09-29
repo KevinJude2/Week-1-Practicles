eb36f26fce5fd3025d26b9ffa40b23268d9edd34
Kevin Saji Jude <Saji_Jude-K@ulster.ac.uk>
2024-09-29 21:02:11 +0100

    Implemented code for Task 1

--- Week1-Task1/Program.cs
+++ Week1-Task1/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+
+
+Console.WriteLine("Please enter in your first name");
+String name = Console.ReadLine();
+
+Console.WriteLine("Please enter in your age");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Hello " + name + " you will be " + (age + 5) + " in 5 years" );
+Console.ReadKey();
+

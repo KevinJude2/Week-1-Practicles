921a160535dfaa6729e9ff2f994e1fec423847d7
Kevin Saji Jude <Saji_Jude-K@ulster.ac.uk>
2024-09-30 22:44:51 +0100

    Implemented code of all Week 1 Tasks.

--- Week1-Task1/Program.cs
+++ Week1-Task1/Program.cs
@@ -1,12 +1,120 @@
 ﻿
+//task1();
+void task1()
+{
+
+    Console.WriteLine("Please enter in your first name");
+    String name = Console.ReadLine();
+
+    Console.WriteLine("Please enter in your age");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(" Hello " + name + " you will be " + (age + 5) + " in 5 years");
+    Console.ReadKey();
+
+}
 
 
-Console.WriteLine("Please enter in your first name");
-String name = Console.ReadLine();
+//task2();
+void task2()
+{
 
-Console.WriteLine("Please enter in your age");
-int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please enter in your age");
+    int age = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(" Hello " + name + " you will be " + (age + 5) + " in 5 years" );
-Console.ReadKey();
+    if (age <= 13)
+    {
+
+        Console.WriteLine("You are a child");
+
+    }
+
+    else if (age > 13 && age < 19)
+
+    {
+
+        Console.WriteLine("You are a teenager");
+
+    }
+
+    else if (age > 19 && age < 110)
+    {
+
+        Console.WriteLine("You are an adult");
+
+    }
+
+    else if (age > 110)
+    {
+
+        Console.WriteLine("You are far too old");
+    }
+}
+
+
+//task3();
+void task3()
+{
+
+    Console.WriteLine("Please enter an integer");
+    int integer = Convert.ToInt32(Console.ReadLine());
+
+    if (integer > 0)
+    {
+
+        Console.WriteLine("This is a positive number");
+
+    }
+
+    else if (integer < 0)
+
+    {
+
+        Console.WriteLine("This is a negative number");
+
+    }
+
+
+    else
+    {
+
+        Console.WriteLine("This number is zero");
+    }
+
+}
+
+//task5();
+void task5()
+{
+
+    Console.WriteLine("Please enter a year:");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+
+    if (num % 2 == 0)
+    {
+
+        Console.WriteLine("This number is even");
+
+    }
+
+    else
+    {
+        Console.WriteLine("This number is odd");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
